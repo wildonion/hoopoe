7559fc00010a5a17fc5dbb437fcacfa253affc53
wildonion <ea_pain@yahoo.com>
2024-05-31 23:04:36 +0330

    a random chars func include no special chars

--- crypter/src/cry.rs
+++ crypter/src/cry.rs
@@ -17,6 +17,18 @@ pub fn gen_random_chars(size: u32) -> String{
     }).collect()
 }
 
+pub fn gen_random_chars_no_special(size: u32) -> String{
+    let mut rng = rand::thread_rng();
+    (0..size).map(|_|{
+        /* converint the generated random ascii to char */
+        let mut c = char::from_u32(rng.gen_range(48..126)).unwrap(); // generating a char from the random output of type u32 using from_u32() method
+        if c == '`'{
+            c = char::from_u32(64).unwrap();
+        }
+        c
+    }).collect()
+}
+
 pub fn get_random_elem<T: Default + Clone + Send + Sync + Sized>(vec: Vec<T>) -> T{
     // making a high entropy seed to create the rng
     let random_chars = gen_random_chars(10);
@@ -90,7 +102,8 @@ pub mod wannacry{
             hex::encode(
                 wallexerr::misc::Wallet::generate_keccak256_hash_from(
                     &{
-                        // high entropy seed with god level security
+                        // high entropy seed with god level security 
+                        // even the server doesn't know the fucking secret key!
                         let random_chars = gen_random_chars(64);
                         let hash_of_random_chars = wallexerr::misc::Wallet::generate_sha256_from(&random_chars); // generating a 256 bits hash
                         let mut crypto_seeded_rng = rand_chacha::ChaCha20Rng::from_seed(hash_of_random_chars);
