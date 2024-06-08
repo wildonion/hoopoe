13e615215a3060c9c2a45f1d7077a5f0452339a3
wildonion <ea_pain@yahoo.com>
2024-06-08 17:52:06 +0330

    add sessions

--- src/workers/ws/servers/hoop.rs
+++ src/workers/ws/servers/hoop.rs
@@ -12,7 +12,8 @@ use crate::consts::PING_INTERVAL;
 
 pub struct HoopServer{
     pub app_storage: std::option::Option<Arc<Storage>>, 
-    pub rooms: HashMap<String, HashSet<Addr<HoopSession>>>  
+    pub rooms: HashMap<String, HashSet<Addr<HoopSession>>>,
+    pub sessions: HashSet<Addr<HoopSession>>
 }
 
 impl Actor for HoopServer{
@@ -34,7 +35,22 @@ impl Actor for HoopServer{
 impl HoopServer{
 
     pub fn new(app_storage: std::option::Option<Arc<Storage>>) -> Self{
-        Self { app_storage, rooms: Default::default() }
+        Self { app_storage, rooms: Default::default(), sessions: Default::default() }
+    }
+
+    // sending to all sessions
+    pub async fn broadcast_msg(&self){
+
+    }
+
+    // sending to all clients in the passed room
+    pub async fn send_msg_to_room(&self, room: &str){
+
+    }
+
+    // sending to an specific actor 
+    pub async fn send_msg_to_actor(&self, addr: Addr<HoopSession>){
+
     }
 
 }
