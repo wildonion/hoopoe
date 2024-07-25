dbb15d090987afb4b9ad04173505b9dfad90ec84
wildonion <ea_pain@yahoo.com>
2024-07-25 17:09:01 +0330

    check the task is busy or not before switching the worker into another one

--- src/tasks/mod.rs
+++ src/tasks/mod.rs
@@ -4,13 +4,25 @@ use crate::*;
 use interfaces::task::TaskExt;
 use types::Job;
 
-// async tasks or jobs that will be executed in 
-// the background inside a lightweight thread of 
-// execution using tokio::spawn() task scheduler 
-// and jobq based channels; actor workers will run 
-// these in their own execution context.
 
 
+/* 
+    async tasks or jobs that will be executed in the background inside a lightweight thread of 
+    execution using tokio::spawn() task scheduler and jobq based channels; actor workers will run 
+    these tasks in their own execution context.
+    typically any instance of the Task must contains:
+        - the future job itself
+        - the sender to send the result of executed task to the channel for using outside of the thread
+        - a thread safe (Mutex) worker as the background worker thread to execute the future job in it
+        - a locker to lock the task when it's executing the task 
+    the task can be awaited to complete its future or aborted during the dropping process of the
+    task instance, tokio::spawn() is the backbone of each background worker, it gets a future and 
+    move it into a lightweight thread of execution. 
+    lock the instance using the lock field to check that if we're doing some heavy process or not
+    then in switching the task or doing other heavy process check the lock that if the instance 
+    is already locked or not also we should lock the worker if we want to execute something in the 
+    background worker of the instance thread to tell obj caller that the worker is busy rn. 
+*/
 #[derive(Clone, Debug)]
 pub struct Task<J, S> where // J is a Future object and must be executed with Box::pin(job);
     J: std::future::Future + Send + Sync + 'static + Clone,
@@ -31,9 +43,10 @@ pub struct Task1<J: std::future::Future<Output = O>, S, O> where // J is a Futur
     pub status: TaskStatus,
     pub name: String, // like send_mail task 
     pub job: J, // use Box::pin(job) to pin it into the ram and call it; the function that needs to get executed 
+    pub job_tree: Vec<Task1<J, S, O>>,
     pub sender: tokio::sync::mpsc::Sender<S>, // use this to send the result of the task into the channel
     pub worker: std::sync::Mutex<tokio::task::JoinHandle<O>>, // execute the task inside the background worker, this is a thread which is safe to be mutated in other threads 
-    pub lock: tokio::sync::Mutex<()>, // the task itself is locked and can't be used by other threads
+    pub lock: std::sync::Mutex<()>, // the task itself is locked and can't be used by other threads
 }
 
 impl<O, J: std::future::Future<Output = O> + Send + Sync + 'static + Clone, S> Task1<J, S, O> 
@@ -45,6 +58,7 @@ impl<O, J: std::future::Future<Output = O> + Send + Sync + 'static + Clone, S> T
             name: String::from("KJHS923"),
             job: job.clone(),
             sender,
+            job_tree: vec![],
             worker: { // this is the worker that can execute the task inside of itself, it's basically a lightweight thread
                 std::sync::Mutex::new(
                     tokio::spawn(job)
@@ -62,11 +76,30 @@ impl<O, J: std::future::Future<Output = O> + Send + Sync + 'static + Clone, S> T
         sender.send(d).await;
     }
 
+    pub fn is_busy(&mut self) -> bool{
+        self.lock.try_lock().is_err() // is_err() can be either true or false, trying to acquire the lock
+    }
+
     pub async fn switch_task(&mut self, taks: J){
+        
+        // wailt until the lock gets freed cause we're pushing tasks into the tree 
+        // if we slide down into the while loop means the method returns true which
+        // means the lock couldn't get acquired
+        while self.is_busy(){ 
+            tokio::time::sleep(tokio::time::Duration::from_secs(10)).await;
+        }
 
         let mut get_worker = self.worker.lock().unwrap();
         (*get_worker) = tokio::spawn(taks);
 
+    }
+
+    pub fn push(mut self, tasks: Vec<Task1<J, S, O>>) -> Task1<J, S, O>{
+        
+        // lock the instance to push tasks into the tree
+        self.lock.lock().unwrap();
+        self.job_tree.extend(tasks);
+        self
     }
 
     // task lifecycles
@@ -80,8 +113,8 @@ impl<O, J: std::future::Future<Output = O> + Send + Sync + 'static + Clone, S> T
 impl<O, J: std::future::Future<Output = O> + Send + Sync + 'static + Clone, S> Drop for Task1<J, S, O> where 
     O: Send + Sync + 'static{
     fn drop(&mut self) { // use std::sync::Mutex instead of tokio cause drop() method is not async 
-        if let Ok(fut) = self.worker.lock(){
-            fut.abort(); // abort the current future inside the joinhandle
+        if let Ok(job) = self.worker.lock(){
+            job.abort(); // abort the current future inside the joinhandle
         }
     }
 }
@@ -136,5 +169,4 @@ impl<J: std::future::Future<Output: Send + Sync + 'static> +
         tokio::spawn(job); // job is of type future, we're executing it inside another free thread
 
     }
-
 }
