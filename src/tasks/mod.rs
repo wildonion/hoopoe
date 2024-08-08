df6b1f36c2c4c7fc6f66018c316b4ab50cac9554
wildonion <ea_pain@yahoo.com>
2024-08-08 15:11:38 +0330

    execute tasks in bfs order

--- src/tasks/mod.rs
+++ src/tasks/mod.rs
@@ -26,39 +26,32 @@ use types::Job;
     is already locked or not also we should lock the worker if we want to execute something in the 
     background worker of the instance thread to tell obj caller that the worker is busy rn. 
 */
-#[derive(Clone, Debug)]
-pub struct Task<J, S> where // J is a Future object and must be executed with Box::pin(job);
-    J: std::future::Future + Send + Sync + 'static + Clone,
-    J::Output: Send + Sync + 'static
-{
-    pub status: TaskStatus,
-    pub name: String, // like send_mail task 
-    pub job: J, // use Box::pin(job) to pin it into the ram and call it; the function that needs to get executed 
-    pub sender: tokio::sync::mpsc::Sender<S>, // use this to send the result of the task into the channel
-}
 
 // future or task, sender, background worker and lock 
-pub struct Task1<J: std::future::Future<Output = O>, S, O> where // J is a Future object and must be executed with Box::pin(job);
+#[derive(Debug)]
+pub struct Task<J: std::future::Future<Output = O>, S, O> where // J is a Future object and must be executed with Box::pin(job);
     J: std::future::Future + Send + Sync + 'static + Clone,
     O: Send + Sync + 'static,
     J::Output: Send + Sync + 'static
 {
     pub status: TaskStatus,
+    pub id: String,
     pub name: String, // like send_mail task 
     pub job: J, // use Box::pin(job) to pin it into the ram and call it; the function that needs to get executed 
-    pub job_tree: Vec<Task1<J, S, O>>,
-    pub sender: tokio::sync::mpsc::Sender<S>, // use this to send the result of the task into the channel
+    pub job_tree: Vec<Task<J, S, O>>,
+    pub sender: tokio::sync::mpsc::Sender<S>, // use this to send the result of the task into the channel to share between other lightweight thread workers
     pub worker: std::sync::Mutex<tokio::task::JoinHandle<O>>, // execute the task inside the background worker, this is a thread which is safe to be mutated in other threads 
     pub lock: std::sync::Mutex<()>, // the task itself is locked and can't be used by other threads
 }
 
-impl<O, J: std::future::Future<Output = O> + Send + Sync + 'static + Clone, S> 
-    Task1<J, S, O> 
+impl<O, J: std::future::Future<Output = O> + Send + Sync + 'static + Clone, S: Sync + Send + 'static> 
+    Task<J, S, O> 
     where O: Send + Sync + 'static{
 
     pub async fn new(job: J, sender: tokio::sync::mpsc::Sender<S>) -> Self{
         let task = Self{
             status: TaskStatus::Initializing,
+            id: Uuid::new_v4().to_string(),
             name: String::from("KJHS923"),
             job: job.clone(),
             sender,
@@ -82,6 +75,12 @@ impl<O, J: std::future::Future<Output = O> + Send + Sync + 'static + Clone, S>
 
     pub fn is_busy(&mut self) -> bool{
         self.lock.try_lock().is_err() // is_err() can be either true or false, trying to acquire the lock
+    }
+
+    pub async fn spawn(&self){
+
+        let job = self.job.clone();
+        tokio::spawn(job);
     }
 
     // method to execute the job in the task worker
@@ -113,7 +112,7 @@ impl<O, J: std::future::Future<Output = O> + Send + Sync + 'static + Clone, S>
 
     }
 
-    pub fn push(mut self, tasks: Vec<Task1<J, S, O>>) -> Task1<J, S, O>{
+    pub fn push(mut self, tasks: Vec<Task<J, S, O>>) -> Task<J, S, O>{
         
         // lock the instance to push tasks into the tree
         self.lock.lock().unwrap();
@@ -129,7 +128,7 @@ impl<O, J: std::future::Future<Output = O> + Send + Sync + 'static + Clone, S>
 }
 
 // once the task gets dropped drop any incomplete futures inside the worker 
-impl<O, J: std::future::Future<Output = O> + Send + Sync + 'static + Clone, S> Drop for Task1<J, S, O> where 
+impl<O, J: std::future::Future<Output = O> + Send + Sync + 'static + Clone, S> Drop for Task<J, S, O> where 
     O: Send + Sync + 'static{
     fn drop(&mut self) { // use std::sync::Mutex instead of tokio cause drop() method is not async 
         if let Ok(job) = self.worker.lock(){
@@ -147,18 +146,14 @@ pub enum TaskStatus{
 }
 
 type FutureTraitObject<O: Send + Sync + 'static> = std::pin::Pin<Box<dyn std::future::Future<Output = O> + Send + Sync + 'static>>;
-impl<J: std::future::Future + Send + Sync + 'static + Clone, S> 
-    Task<J, S> 
+impl<J: std::future::Future<Output = O> + Send + Sync + 'static + Clone, 
+     S: Send + Sync + 'static, O: Send + Sync + 'static> 
+    Task<J, S, O> 
     where J::Output: Send + Sync + 'static{
 
-    // spawn an async task inside tokio threads upon tokio scheduler
-    pub async fn spawn(&self){
-        self.execute(String::from("stringified task")).await;
-    }
-
     pub async fn spawn_task< // none assoc method
-        O: Send + Sync + 'static, R: Send + Sync + 'static,
         F: std::future::Future<Output = O> + Send + Sync + 'static,
+        R: Send + Sync + 'static,
         V: FnOnce(O) -> R + Send + Sync + 'static
         >(
             fut1: F,
@@ -173,8 +168,8 @@ impl<J: std::future::Future + Send + Sync + 'static + Clone, S>
 
 }
 
-impl<J: std::future::Future<Output: Send + Sync + 'static> + 
-     Send + Sync + 'static + Clone, S> TaskExt<String> for Task<J, S>
+impl<O: Send + Sync + 'static, J: std::future::Future<Output = O> + 
+     Send + Sync + 'static + Clone, S> TaskExt<String> for Task<J, S, O>
     where 
         J::Output: Send + Sync + 'static, 
         <J as std::future::Future>::Output: Send + Sync + 'static{
@@ -182,7 +177,7 @@ impl<J: std::future::Future<Output: Send + Sync + 'static> +
     type State = String;
     type Task = Self;
     
-    async fn execute(&self, t: String) {
+    async fn execute_this(&self, t: String) {
          
         let this = self.clone();
         let job = this.clone().job.clone();
@@ -191,35 +186,66 @@ impl<J: std::future::Future<Output: Send + Sync + 'static> +
     }
 }
 
-//          a thread safe task tree
-// use smart pointers to break the cycle of self ref 
-// types, in here we're creating a node for the entire 
-// task tree which contains a reference to the itself
-pub struct TaskNode<
+/* ----------------------------------------------------- */
+//          a thread safe task tree executor
+/* ----------------------------------------------------- 
+|   use smart pointers to break the cycle of self ref 
+|   types, in here we're creating a node for the entire 
+|   task tree which contains a reference to the itself
+|   it gets executed in BFS order.
+|
+*/
+
+pub struct TaskTree<
     J: std::future::Future<Output = O> + Send + Sync + 'static + Clone, 
     S, O: Send + Sync + 'static>{
-    pub key: String,
-    pub value: Task1<J, S, O>,
+    // wrap it around mutex to share the task between threads cause we
+    // want to execute the task in a light thread without blocking so 
+    // we need to move the reference of the task into the thread which 
+    // can be done via mutex since it's an smart pointer for sharing data
+    // safely between threads
+    pub task: tokio::sync::Mutex<Task<J, S, O>>, 
     pub weight: std::sync::atomic::AtomicU8,
-    pub parent: std::sync::Arc<TaskNode<J, S, O>>, // the parent itself
-    pub children: std::sync::Mutex<std::sync::Arc<TaskNode<J, S, O>>> // vector of children
+    pub parent: std::sync::Arc<TaskTree<J, S, O>>, // the parent itself
+    pub children: std::sync::Mutex<Vec<std::sync::Arc<TaskTree<J, S, O>>>> // vector of children
 }
 
-impl<J: std::future::Future<Output = O> + Send + Sync + 'static + Clone, S, O: Send + Sync + 'static> 
-    TaskNode<J, S, O>{
+impl<J: std::future::Future<Output = O> + Send + Sync + 'static + Clone + std::fmt::Debug, 
+    S: std::fmt::Debug + Send + Sync + 'static, O: std::fmt::Debug + Send + Sync + 'static> 
+    TaskTree<J, S, O>{
     
-    /* execute tasks in bfs and dfs order */
+    // execute all tasks in bfs order in none binary tree
+    pub fn execute_all_tasks(&mut self, root: std::sync::Arc<TaskTree<J, S, O>>){
+        let mut queue = vec![root]; 
+        while !queue.is_empty(){
+            let get_node = queue.pop(); // pop the child out
+            if get_node.is_some(){
+                let node = get_node.unwrap();
+                let cloned_node = node.clone();
+                
+                // executing the task in the background light thread in a none 
+                // blocking io manner, we tried to acquire the lock of the value 
+                // in a separate thread to avoid blocking the current thread for doing so
+                tokio::spawn(async move{
+                    let mut task = cloned_node.task.lock().await;
+                    println!("[*] executing the task with id: {:?}", task.id);
+                    // this method contains a locking process on the task itself so it's better
+                    // to execute it in a separate light io thread
+                    task.execute().await;
+                });
 
-    pub fn executex_bfs(&mut self){
-        // execute all tasks in bfs order
-        // level order traversal
-
+                let get_children = node.children.try_lock().unwrap();
+                let children = get_children.to_vec();
+                for child in children{
+                    queue.push(child);
+                }
+            }
+        }
     }
 
-    pub fn execute_dfs(&mut self){
-        // execute all tasks in dfs order
-        // inorder, preorder, postorder traversal
-        
+    pub fn push_task(&mut self, child: std::sync::Arc<TaskTree<J, S, O>>){
+        let mut get_children = self.children.try_lock().unwrap();
+        (*get_children).push(child);
     }
 
 }
