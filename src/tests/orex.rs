fefbf41a89f8e3f1670c33441ff8dc8c6e06ebc1
wildonion <ea_pain@yahoo.com>
2024-08-15 00:05:58 +0330

    coding condvar logic

--- src/tests/orex.rs
+++ src/tests/orex.rs
@@ -1,5 +1,9 @@
 
 
+use std::thread;
+
+use futures::SinkExt;
+use tokio::sync::mpsc;
 use crate::*;
 
 /* ------------------------------------------- */
@@ -11,6 +15,39 @@ use crate::*;
     ----------------------------------------------------------------------
               Golang and Rust (goroutines and futures)!
     ==========>>>==========>>>==========>>>==========>>>==========>>>=====
+    light threads are background threads used to execute async tasks 
+    in a none blocking manner and without blocking the eventloop.
+    don't block in the tokio threads cause it's being used to execute 
+    future async io tasks without waiting on them in the thread they're 
+    inside, instead it executes other async io tasks in a none blocking 
+    manner, the runtime scheduler pauses the execution of async io tasks 
+    in that lightweigh thread and continue with executing other tasks, 
+    the scheduler however will resume the paused task later when the awaited 
+    operation (like I/O) is ready, meanwhile there will be used some placeholder
+    of the async task in places we're calling it. golang runtime however
+    block the thread which the task is executing in a lightweight thread 
+    but it doesn't block the main thread of the app other takss will be
+    executed in other threads when the light thread is busy, the runtime 
+    switches into other threads for new incoming task.  
+    in golang every goroutine is a lightweight thread used to execute 
+    tasks inside of it, each goroutine has its own queue uses it to 
+    pop the task out of it for execution the runtime however do this
+    on the other hand tokio spawn threadpool are lightweight threads
+    used to execute none blocking io async tasks, each thread in tokio
+    has its own queue which will be managed by the tokio runtime for 
+    async task execution it's noticable that we must not block the light
+    thread cause they're being used to execute none blocking tasks also
+    we must make sure that we're separating the cpu threadpool from the
+    io one to avoid the costs of overhead blocking. DON'T BLOCK THE IO
+    THREADS, if you need a blocking operation use os thread instead.
+    cpu threadpool can be used for executing heavy math and cryptography
+    computational process in which the thread might gets blocked.
+    joining on os threads block the thread but this is not true about 
+    the lightweight ones in tokio, when we join on a tokio thread it
+    await for the result asyncly in a none blocking manner.
+    locking inside thread will always block the thread to avoid mutating
+    the data by other threads at the same time so it's better to use 
+    channels for atomic syncing.
     future objects in Rust are async taks that will be execute in a free 
     thread by the async runtime based on task or work stealing algo.
     in Rust async task in lightweight thread execution is handled by the 
@@ -242,6 +279,15 @@ use crate::*;
 
 pub async fn atomic_map_demo(){
 
+
+    // a blocking threadpool using rayon used for cryptography operations
+    rayon::spawn(move ||{
+        let mut wallet = wallexerr::misc::Wallet::new_ed25519();
+        let prv = wallet.clone().ed25519_secret_key.unwrap(); // calling unwrap() takes the ownership of the type use clone() or as_ref()
+        let sig = wallet.self_ed25519_sign("data", &prv);
+    });
+    
+    // a none blocking threadpool using tokio used for async future io tasks
     // ----- joining thread vs executing in the background -----
     tokio::spawn(async move{
         
@@ -391,6 +437,10 @@ pub async fn atomic_map_demo(){
 
 
     /* 
+        if you want to share a type between threads for either mutating it or call 
+        a mutable method of it you need to wrap it around a Mutex or RwLock and if 
+        it's not cloneable we must put it inside Arc, doing so avoids deadlock and 
+        race conditions which can be happened by two threads at the same time.
         mutex block the threads to avoid mutating data by other threads at the same time, lock gets released 
         once the mutex goes out of scope:
         atomic syncing with blocking: arc mutex send sync static 
@@ -400,6 +450,7 @@ pub async fn atomic_map_demo(){
         mutable reference
         atomic syncing and sharing data between lightweight threads of task execution:
         1 - channels (fastest) -> data must be send sync and have valid lifetimes during thread execution or move the entire ownership
+            if channel is unbuffered it's called sync channel and means there would be no async io process
         2 - static lazy arc mutex or rwlock (slowest) -> this can be mutated globally across the app at any time
         NOTE - a lightweight thread with its task must be executed in the background and 
                get any data inside of it using channels, there is another way to get the
@@ -538,4 +589,287 @@ pub async fn test_code_order_exec(){
         sleep2().await;
     });
     
+}
+
+pub async fn ExecutorEventLoopPlayGround(){
+
+    // actor worker streaming with while let some, ws, atomic addr, mailbox, rpc, redis, rmq)
+    // actor worker task execution with io vs os threads (lazy static &mut rc arc mutex rwlock select spawn channels scheudler interval)
+    // thread queue and runtime scheduler to pop the task out of the queue, send sync static separate io threads vs os threads thread joining
+    
+    
+    // concurrency      : (&mut <-> Mutex RwLock)
+    // threadpool       : light or os threads, eventloop (threadpool channel queue to handle events in a loop see actor.rs with threadpool)
+    // atomic syncing   : channels mutex rwlock arc select
+    // future objects   : async io task, thread joining on main thread
+    // purchase locking : lock the object when someone is minting it using select mutex spawn
+    
+    /*
+        simple executor without threadpool but with eventloop:
+        task is a unit of work thread or groutine that must be executed 
+        by runtime executor by poping it out of the queue, where each task 
+        runs to completion before the next task starts.
+        it's similar to how tokio event loop manages async io future object 
+        using an eventloop in its lightweight thread of execution
+    */
+
+    use tokio::sync::{mpsc::channel, Mutex};
+    enum EventData{
+        Task(Task),
+        Quit
+    }
+    // traits as separate objects must be behind dyn keyword 
+    // and boxed into the heap cause they're unsized
+    type Function = fn();
+    struct Fucntions<F: FnOnce() + Send + Sync + 'static, 
+        A: std::future::Future<Output = String> + Send + Sync + 'static>{
+        pub func: Function,
+        pub cls: F,
+        pub boxed_cls: Box<dyn FnOnce() + Send + Sync + 'static>,
+        pub fut: A,
+        pub fut1: std::pin::Pin<Box<dyn std::future::Future<Output = String> + Send + Sync + 'static>>
+    }
+    struct Task{
+        name: String,
+        func: Box<dyn FnOnce() + Send + Sync + 'static>,
+        fut: Option<std::pin::Pin<Box<dyn std::future::Future<Output=String> + Send + Sync + 'static>>>,
+    }
+    impl Task{
+        fn new<F: FnOnce() + Send + Sync + 'static>(name: String, func: F) -> Self{
+            Task { name, func: Box::new(func), fut: None }
+        }
+        fn run(self){ // don't use &self in here cause we can't move out of it since func is not cloned
+            (self.func)()
+        }
+    }
+    struct Executor{
+        pub tx: mpsc::Sender<EventData>,
+        pub rx: mpsc::Receiver<EventData>
+    }
+    impl Executor{
+        fn new(buffer_size: usize) -> Self{
+            let (tx, rx) = tokio::sync::mpsc::channel::<EventData>(buffer_size);
+            Executor{
+                tx,
+                rx
+            }
+        }
+        fn spawn<F: FnOnce() + Send + Sync + 'static>(&mut self, name: String, func: F){
+            self.tx.send(EventData::Task(Task::new(name, func)));
+        }
+        async fn run(&mut self){
+            // await puases the execution and run the future until it completes
+            // futures don't block the thread of execution
+            while let Some(event_data) = self.rx.recv().await{
+                match event_data{
+                    EventData::Task(task) => {
+                        log::info!("executing the task with name: {}", task.name);
+                    },
+                    EventData::Quit => {
+                        break;
+                    },
+                    _ => {
+                        panic!("invalid event data, event loop is panicked");
+                    }
+                }
+            }
+        }
+    }
+
+}
+
+pub fn StockPriceEvent(){
+
+    /* --------------------------------------------------------------------
+        condvar is used to check that either a condition var is met
+        inside a mutex or not, this will block the mutex thread by 
+        waiting on it until this cond var receives a notification in 
+        somewhere else.
+        CondVar: block the thread such that it consumes no CPU time while
+        waiting for an event to occur, other threads can do their jobs
+        functions in this module will block the current thread of 
+        execution, condvars are typically associated with a boolean 
+        predicate (a condition) and a mutex, the predicate is always 
+        verified inside of the mutex before determining that a 
+        thread must block.
+        condvar blocks the mutex thread by waiting until new changes 
+        is received which has notified in other threads.
+        what we're doing here is basically we're monitoring a stock price in 
+        a safe manner in such a way that we're creating 10 threads, each of 
+        them wants to mutate the price of the stock instance but the actual 
+        instance is wrapped through a mutex and has a condvar, the updating 
+        process is happened like we start by locking on the stock instance 
+        then call update price method after that notify the condvar that the 
+        value of the price of the stock instance has changed so the notification 
+        process is happening inside each thread. 
+        then in the function we're calling the wait for release which lock the 
+        stock again and checks its price against a limit causes to block the 
+        main thread until the price of the stock is smaller than the limit, it 
+        depends on the update price every time the update price function update 
+        the price of the stock a notif gets triggered which will be checked 
+        by the wait for release method to check the price agains the limit this 
+        process continues constantly the main thread is blocked until the price 
+        reaches a higher amount than the limit.
+    */
+
+
+    use std::sync::{Arc, Mutex, Condvar};
+
+    struct Buffer<T>{
+        pub data: Arc<Mutex<Vec<T>>>,
+        pub size: usize
+    }
+
+    #[derive(Debug, Clone)]
+    struct Stock{
+        name: String,
+        price: f64
+    }
+    impl Stock{
+        fn new(name: &str, price: f64) -> Self{
+            Self { name: name.to_string(), price }
+        }
+        fn getPrice(&self) -> f64{
+            self.price
+        }
+        fn getName(&self) -> &str{ // ret pointer, use the lifetime of the self
+            &self.name
+        }
+        fn updatePrice(&mut self, new_price: f64){
+            self.price = new_price;
+        }
+    }
+
+    // worker, locker
+    struct Monitor{
+        pub event: std::sync::Mutex<Stock>,
+        pub events: Option<Buffer<Stock>>,
+        pub event_signal: Condvar,
+        pub std_worker: thread::JoinHandle<()>,
+        pub tokio_worker: tokio::task::JoinHandle<()>,
+        pub locker: std::sync::Mutex<()>
+    }
+
+    impl Monitor{
+        fn new(init_event: Stock) -> Self{
+            Self {
+                events: None,
+                event: std::sync::Mutex::new(init_event), 
+                event_signal: Condvar::new(), 
+                std_worker: thread::spawn(move ||{}), 
+                tokio_worker: tokio::spawn(async move{}),
+                locker: std::sync::Mutex::new(())
+            }
+        }
+
+        fn update_price(&self, new_price: f64){
+            let mut get_stock = self.event.lock().unwrap();
+            (*get_stock).updatePrice(new_price);
+
+            // wakes up one blocked mutex thread on this condvar
+            // we notify the condvar that the stock price is changed
+            // the update_price method locks the Stock instance, updates 
+            // its price, and then calls notify_one() on the Condvar. 
+            // this notifies any thread waiting or blocking on the Condvar 
+            // that the stock price has changed.
+            self.event_signal.notify_one()
+        }
+
+        /* 
+            once the price of the locked stock reaches the limit we wait, wait blocks the current thread 
+            until this condition variable receives a notification which will be triggered inside the 
+            update_price method means that the price of the stock has changed and we need to block the 
+            mutex thread again until we reaches the limit again for the stock price.
+            in the wait_for_release() method, we lock the Stock object. it then enters a loop where 
+            it continually checks if the price of the Stock is less than a certain limit. if the price 
+            is less than the limit, the method calls the self.event_signal.wait(get_stock) 
+            method. this block the current (main) thread of the mutex, until another thread calls notify_one() 
+            or notify_all() on the same Condvar
+            the consequence of this, is that if the price of the Stock is initially less than the limit, 
+            this method will block the current (main) thread until the price increases to the limit or above. 
+            this will allow other threads to update the price of the Stock while the current (main) thread is 
+            blocked. once the prices reaches the limit, the wait() method will return. the method will 
+            exit the loop and continue executing.
+            using a Condvar in this way, we can effectively manage access to the Stock. By using the 
+            wait_for_release() method, the main thread waits for the price of the Stock to reach a certain 
+            limit before proceeding. this is useful in scenarios where the order of operations matters, 
+            for example when one operation depends on the result of another. example scenarios would be 
+            things like managing stocks, purchasing a product, or a warehouse ledger system.
+        */
+        fn wait_for_release(&self){
+            let limit = 115.0;
+            let mut get_stock = self.event.lock().unwrap();
+            while get_stock.getPrice() < limit{ // we block and wait as long as the the price is smaller than the limit
+                get_stock = self.event_signal.wait(get_stock).unwrap();
+            }
+
+        }
+
+    }
+
+
+    // testing:
+    // basically in here we're updating the price 
+    // in 10 threads and block the main thread if 
+    // the price is smaller than the limit until 
+    // we notify the blocked thread by the condvar 
+    // that the price value is changed, then there 
+    // would be no need to wait for the notif until
+    // another thread tries to update the price.
+    // we spawn the update_price() method inside 10
+    // threads then block the main thread if the price
+    // is not met the limit finally we iterate through 
+    // all the threads to join them on the main thread
+    // and wait for them to finish.
+    // waiting in os threads means blocking the thread 
+    // until we get the result.
+
+    
+    // arc monitor to move it between threads
+    let monitor = Arc::new(Monitor::new(Stock::new("DOGTOKEN", 100.0)));
+    let threads = (0..10)
+        .map(|counter|{
+            let cloned_monitor = monitor.clone();
+            // we'll update the price of the monitor instance in a separate 10 of threads
+            thread::spawn(move ||{
+                cloned_monitor.update_price(110.0 + 2.0*(counter as f64));
+            })
+        })
+        .collect::<Vec<_>>(); // if you don't know the type use _
+
+    // we'll check the price of the stock against the limit 
+    // if it was less than the limit then we'll block the main 
+    // thread until the notifier notify the condvar in another 
+    // thread with a new value of the price, then we'll wait and 
+    // block the thread until the price reaches higher than the limit again.
+    // ------- this blocks the main thread -------
+    monitor.wait_for_release(); 
+
+    // join on all threads in main thread to execute the stock price task
+    for thread in threads{
+        thread.join().unwrap();
+    }
+
+    let final_value = monitor.event.lock().unwrap();
+    println!("final value of the stock is {:?}", final_value);
+
+
+    // wait_for_release() method blocks the main thread until we reach
+    // the limit, or receives a notification from the condvar which might
+    // happens in another thread by updating the price of the stock.  
+
+    /* 
+        product minting: 
+           - condvar with tokio spawn threads: use a condvar with a mutex and lock the product id in a tokio
+                      io thread then check while the product is still locked or its state is not minted yet, 
+                      we'll block the current thread until the notifier notifies the condvar once the 
+                      product gets minted successfully and its state changed to minted then we'll remove it 
+                      from the lock_ids.
+           - channels with tokio spawn thrads: use a mutex and lock the product id in a tokio io thread
+                      then send a true flag to a channel if the product id is being locked then start minting 
+                      product in another tokio io thread finally use select to control the flow of execution 
+                      of each joinhandle task.
+    */
+    
+
 }
