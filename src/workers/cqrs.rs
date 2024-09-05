df374654317fdad0b5cf2a720bf9bc26d3086f04
wildonion <ea_pain@yahoo.com>
2024-09-05 09:18:23 +0330

    debug and display are now implemented

--- src/workers/cqrs.rs
+++ src/workers/cqrs.rs
@@ -2,30 +2,57 @@
 
 
 
-use std::{error::Error, fmt::Display};
+use std::{error::Error, fmt::{Display, Write}};
 use thiserror::Error;
 pub mod mutators;
 pub mod accessors;
 
 // the Error, Debug, From and Display traits must be implemented for the CqrsError and its subtypes
+// the Error trait will be implemented by the thiserror Error macro for each type annotated with this macro
 
-#[derive(Error, Debug)]
+#[derive(Error)]
 pub struct CqrsError{
     kind: CqrsErrorKind,
     time: i64
 }
 
-#[derive(Error, Debug)]
+#[derive(Error)]
 pub enum CqrsErrorKind{
-    Mutator(String),
-    Accessor(String)
+    Mutator(String), // the cause of the mutator actor error is of type string
+    Accessor(String) // the cause of the accessor actor error is of type string
 }
 
+
+// Debug trait is used to extend and change the way of 
+// debugging an instance of a type to the console
+impl std::fmt::Debug for CqrsError{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(f, "{}", self)?;
+        if let Some(source) = self.source(){
+            writeln!(f, "Debug Error: {}", source)?;
+        }
+        Ok(())
+    }   
+}
+
+impl std::fmt::Debug for CqrsErrorKind{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(f, "{}", self)?;
+        if let Some(source) = self.source(){
+            writeln!(f, "Debug Error: {}", source)?;
+        }
+        Ok(())
+    }   
+}
+
+
+// Debug trait is used to extend and change the way of 
+// logging and printing an instance of a type to the console
 impl Display for CqrsError{
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}", self)?;
         if let Some(source) = self.source(){ // source is of type trait which is used for dynamic dispatching
-            writeln!(f, "caused by the source: {:?}", source)?;
+            writeln!(f, "Display Error: {:?}", source)?;
         }
         Ok(())
     }
@@ -35,7 +62,7 @@ impl Display for CqrsErrorKind{
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}", self)?;
         if let Some(source) = self.source(){ // source is of type trait which is used for dynamic dispatching
-            writeln!(f, "caused by the source: {:?}", source)?;
+            writeln!(f, "Display Error: {:?}", source)?;
         }
         Ok(())
     }
